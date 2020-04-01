eaf0e4923da41a02a9102108f9c7124b84b8ae29
Nenouchka <helene.morel@gmail.com>
2020-04-01 12:00:59 +0200

    fin chat-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact'
+
+
+const emily = {
+  name: "Emily Bell",
+  img: "https://randomuser.me/api/portraits/women/81.jpg",
+  status: 'Online'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Contact  {... emily}/>
+    </>
   );
 }
 
