60ba6469167d16ce3b771e7cd2efdb926acb1e56
Nenouchka <helene.morel@gmail.com>
2020-04-12 11:32:58 +0200

    création branche contact-state et fin React5

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,30 @@
 import React from "react";
 import "./Contact.css";
 
-const Contact = ({ name, avatar, online }) => {
-  return (
-    <>
-      <div className="Contact">
-        <img className="avatar" src={avatar} alt="avatar" />
-        <div>
-          <p className="name">{name}</p>
-          <div className="status">
-            <p className={online ? "status-online" : "status-offline"} />
-            <p className="status-text">{online ? "online" : "offline"}</p>
+class Contact extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      online: props.online
+    }
+  }   
+
+  render() {
+    return (
+      <>
+        <div className="Contact">
+          <img className="avatar" src={this.props.avatar} alt="avatar" />
+          <div>
+            <p className="name">{this.props.name}</p>
+            <div className="status" onClick={() => this.setState({online: !this.state.online})}>
+              <p className={this.state.online ? "status-online" : "status-offline"} />
+              <p className="status-text">{this.state.online ? "online" : "offline"}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default Contact;
